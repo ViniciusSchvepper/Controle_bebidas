0ed49501ef8466717e71746b55de6b6bfff98e5d
Vinicius Schvepper <125335561+ViniciusSchvepper@users.noreply.github.com>
2023-08-24 19:23:22 -0300

    Update controle.py

--- controle.py
+++ controle.py
@@ -4,7 +4,7 @@ import psycopg2
 hostname = 'localhost'
 database = 'bebidas'
 username = 'postgres'
-pwd = 'postgres'
+pwd = 'pwd'
 port_id = 5432
 conn = None
 cursor = None
@@ -16,86 +16,79 @@ try:
         user = username,
         password = pwd,
         port = port_id
-        )
+    )
     cursor = conn.cursor()
-    print(f"Conectado com sucesso ao banco de dados {database} do PostgreSQL!")
+    print(f'Conectado com sucesso ao banco de dados {database}')
 
-    print('O que você deseja fazer?')
-    print('Adicionar, remover, consultar, atualizar')
-    acao = input('Qual a opção escolhida? ').lower()
-
-    if acao == 'adicionar':
-        nome_bebida = input('Qual a bebida você deseja adicionar? ').upper()
-        procurar_nome = f'''SELECT * FROM bebidas 
-                            WHERE nm_bebidas = '{nome_bebida}' '''
-        cursor.execute(procurar_nome)
+    acao = input('O que você deseja fazer?\n'
+          'Adicoinar, remover, atualizar, consultar ou sair?\n').lower()
+    
+    def executar():
+        cursor.execute(acao)
         conn.commit()
-        resultados = cursor.fetchall()
-        if resultados:
-            ja_existe = input('Esta bebida ja existe, invês disso gostaria de atualizar seu valor? ').lower()
-            if ja_existe == 'sim':
-                valor = input('Digite a quantidade desejada: ')
-                soma = f'''UPDATE bebidas SET qtd_disponivel = qtd_disponivel + {valor}
-                            WHERE nm_bebidas = '{nome_bebida}' '''
-                cursor.execute(soma)
-                conn.commit()
-                print('Quantidade atualizada com sucesso')
-        else:
-            quantidade = input('Qual o valor desejado? ')
-            inserir = f'''INSERT INTO BEBIDAS (nm_bebidas,qtd_disponivel)
-                            VALUES ('{nome_bebida}', '{quantidade}')'''
-            cursor.execute(inserir)
-            conn.commit()
-            print(f'A Bebida {nome_bebida} foi adicionada com sucesso')
+    
+    def soma():
+        quantidade = input('Quantas unidades deseja adicionar?\n')
+        acao = f'''UPDATE bebidas SET qtd_disponivel = qtd_disponivel + {quantidade}
+                    WHERE nm_bebidas = '{nome_bebida}' '''
+        executar()
 
-    elif acao == 'remover':
-        remover_bebida = input('Qual bebida você deseja remover? ').upper()
-        confirmacao_remover = input('Você realmente deseja remover esta bebida? ').lower()
-        if confirmacao_remover == 'sim':
-            deletar = f'''DELETE FROM bebidas
-                        WHERE bebidas.nm_bebidas = '{remover_bebida}' '''
-            cursor.execute(deletar)
-            conn.commit()
-            print(f'A bebida {remover_bebida} foi removida com sucesso!')
-        else:
-            print('Ação não confirmada')
-
-    elif acao == 'consultar':
-        visualizar = '''SELECT * FROM bebidas'''
-        cursor.execute(visualizar)
-        resultado = cursor.fetchall()
-        print('Essas são as bebidas registradas:')
-        for linha in resultado:
-            print(linha)
-
-    elif acao == 'atualizar':
-        nome_bebida = input('De qual bebida você quer atualizar o valor? ').upper()
-        procurar_nome = f'''SELECT * FROM bebidas 
-                            WHERE nm_bebidas = '{nome_bebida}' '''
-        cursor.execute(procurar_nome)
-        conn.commit()
-        resultados = cursor.fetchall()
-        if resultados:
-            soma_diminui = input('Você deseja somar ou diminuir a quantidade? ').lower()
-            valor = input('Qual a quantidade? ')
-            if soma_diminui == 'somar':
-                soma = f'''UPDATE bebidas SET qtd_disponivel = qtd_disponivel + {valor}
-                            WHERE nm_bebidas = '{nome_bebida}' '''
-                cursor.execute(soma)
-                conn.commit()
-                print('Valor atualizado com sucesso!')
-            elif soma_diminui == 'diminuir':
-                diminuir = f'''UPDATE bebidas SET qtd_disponivel = qtd_disponivel - {valor}
+    def checagem():
+        acao = f'''SELECT * FROM bebidas
                                 WHERE nm_bebidas = '{nome_bebida}' '''
-                cursor.execute(diminuir)
-                conn.commit()
-                print('Valor atualizado com sucesso!')
-        else:
-            print('Bebida não encontrada')
+        executar()
+        resultado = cursor.fetchall()
+        return resultado
 
-    else:
-        print('Ação inválida')
-        
+    while acao != 'sair':
+        if acao == 'adicionar':
+            nome_bebida = input('Qual bebida deseja adicionar?\n')
+            checagem()
+
+            if resultado:
+                ja_existe = input('Esta bebida já existe, invês disso gostaria de atualizar seu valor?\n').lower()
+                if ja_existe == 'sim':
+                    soma()
+            else:
+                quantidade = input('Qual o valor desejado?')
+                inserir = f'''INSERT INTO bebidas (nm_bebidas,qtd_diponivel)
+                                VALUES ('{nome_bebida}, '{quantidade}')'''
+                executar()
+
+        elif acao == 'remover':
+            remover = input('Qual bebida você deseja remover? \n')
+            confirmacao = input('Você realmente deseja remover esta bebida? \n')
+            if confirmacao == 'sim':
+                acao = f'''DELETE FROM bebidas
+                            WHERE bebidas.nm_bebidas = '{remover}' '''
+                executar()
+            else:
+                print('Ação não confirmada')
+
+        elif acao == 'atualizar':
+            nome_bebida = input('Qual bebida você deseja atualizar o valor?')
+            resultado = checagem()
+            if resultado:
+                soma_subtrai = input('Você deseja adicionar ou retirar?\n').lower()
+                if soma_subtrai == 'somar':
+                    soma()
+                elif soma_subtrai == 'subtrair':
+                    valor = input('Qual o valor?')
+                    acao = f'''UPDATE bebidas SET qtd_disponivel = qtd_disponivel - {valor}
+                                    WHERE nm_bebidas = '{nome_bebida}' '''
+            else:
+                print('Bebida não encontrada')
+
+        elif acao == 'consultar':
+            checagem()
+            print('Estas são as bebidas registradas: ')
+            for linha in resultado:
+                print(linha)
+        elif acao == 'sair':
+            print('Saindo do sistema!')
+            cursor.close()
+            conn.close()
+
 except Exception as error:
     print(error)
 
